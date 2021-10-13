bab26fed144a882d1830127d7e273e4c8f159c99
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2021-10-13 19:20:43 -0400

    Initial Project Commit. Closes #1

---
+++ src/main/java/org/taljaard/training/trnmicroservbeerservice/TrnMicroServBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.taljaard.training.trnmicroservbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrnMicroServBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrnMicroServBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/taljaard/training/trnmicroservbeerservice/TrnMicroServBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taljaard.training.trnmicroservbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrnMicroServBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
