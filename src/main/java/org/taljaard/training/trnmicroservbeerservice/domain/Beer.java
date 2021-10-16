264a46261dadb7bbc01bb1ae9b1b49226b6d74e5
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2021-10-16 12:35:35 -0400

    Add JPA Entity. Closes #5

---
+++ src/main/java/org/taljaard/training/trnmicroservbeerservice/domain/Beer.java
@@ -0,0 +1,53 @@
+package org.taljaard.training.trnmicroservbeerservice.domain;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
