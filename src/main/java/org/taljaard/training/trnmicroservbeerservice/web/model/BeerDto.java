4b39bf62b80d23b618024f1b98398b8a5e06b10a
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2021-10-17 16:18:56 -0400

    Add Bean Validation. Closes #8

--- src/main/java/org/taljaard/training/trnmicroservbeerservice/web/model/BeerDto.java
+++ src/main/java/org/taljaard/training/trnmicroservbeerservice/web/model/BeerDto.java
@@ -4,6 +4,11 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AccessLevel;
@@ -19,15 +24,29 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime updateDate;
 
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyleEnum beerStyles;
 
+    @Positive
+    @NotNull
     private Long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
 
     private Integer quantityOnHand;
